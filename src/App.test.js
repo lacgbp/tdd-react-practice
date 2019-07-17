236b3be8555c9cb0df6e1c021b81c0fa38b2f63c
Luis Cisneros <luiscisneros@MacBook-Pro-de-Luis.local>
2019-07-16 22:07:48 -0500

    jest and enzyme configure and basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Jest deafult test', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
+
+describe('Enzyme deafult test', () => {
+  it('render snapshot', () => {
+    const snapShot = shallow(<App />);
+    expect(snapShot).toMatchSnapshot();
+  });
+})
