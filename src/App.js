cee632da6c59b391ad26fada020e81eefdd5459e
Luis Cisneros <luiscisneros@MacBook-Pro-de-Luis.local>
2019-07-24 19:23:02 -0500

    basic redux configuration with thunk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ToDo from './components/ToDo';
+import store from './redux/store';
 
 function App() {
   return (
-    <ToDo />
+    <Provider store={store}>
+      <ToDo />
+    </Provider>
   );
 }
 
